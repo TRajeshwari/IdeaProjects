acef39d8022942fe7212c87e98e20b09a71dfe05
Rajeshwari T <ssrajeshwari22@gmail.com>
2022-01-30 19:12:47 +0530

    Adding diff.java

---
+++ src/Diff.java
@@ -0,0 +1,10 @@
+public class Diff {
+
+    public static void main(String[] args) {
+        int a =9;
+        int b =5;
+//        int diff = a-b;
+        System.out.println("Difference is :: "+(a-b));
+    }
+}
+
